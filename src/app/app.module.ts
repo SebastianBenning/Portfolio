f3f961b817a445837ca369d7a4a592b698b19de2
SebastianBenning <benning655@googlemail.com>
2023-03-02 03:22:53 +0100

    aboutme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartscreenComponent } from './startscreen/startscreen.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartscreenComponent
+    StartscreenComponent,
+    AboutmeComponent
   ],
   imports: [
     BrowserModule,
