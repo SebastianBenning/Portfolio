f8abf14e0132c6d3e9df1a8118914402c965a02a
SebastianBenning <benning655@googlemail.com>
2023-03-06 10:14:26 +0100

    mywork

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { StartscreenComponent } from './startscreen/startscreen.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { SkillsetComponent } from './skillset/skillset.component';
+import { MyworkComponent } from './mywork/mywork.component';
+import { JoinComponent } from './join/join.component';
+import { SharkieComponent } from './sharkie/sharkie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartscreenComponent,
     AboutmeComponent,
-    SkillsetComponent
+    SkillsetComponent,
+    MyworkComponent,
+    JoinComponent,
+    SharkieComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sharkie/sharkie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sharkie',
+  templateUrl: './sharkie.component.html',
+  styleUrls: ['./sharkie.component.scss']
+})
+export class SharkieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
